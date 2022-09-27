1d4154048358b16c21516869bd3f98d0d2782fa0
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-09-27 17:01:48 -0500

    Building out constructor, starting validation.

--- src/bank/BankAccount.java
+++ src/bank/BankAccount.java
@@ -1,11 +1,43 @@
 package bank;
 
-abstract class BankAccount {
+public abstract class BankAccount {
 	
 	private long accountNumber;
 	private long[] accountOwners;
 	private double balance;
 	
+	public BankAccount(long accountNumber, double balance, long... accountOwners) throws AccountException
+	{
+		//accountNumber needs to be validated
+		isAccountNumber(accountNumber);
+		isBalance(balance);
+		isAccountOwner(accountOwners);
+		this.accountNumber = accountNumber;
+		//balance needs to be validated
+		this.balance = balance;
+		//accountOwners needs to be validated
+		this.accountOwners = accountOwners;
+	}
 	
+	private boolean isAccountNumber(long input) throws invalidAccountNumberException
+	{
+		if (input < 1 || input > 10000000)
+		{
+			throw new invalidAccountNumberException(input);
+		}
+		
+		return true;
+	}
+	
+	private boolean isBalance(double input) throws invalidMoneyException
+	{
+		double temp = (Math.round(input * 100)) / 100.0;
+		if (temp != input)
+		{
+			throw new invalidMoneyException(input);
+		}
+		
+		return true;
+	}
 
 }
